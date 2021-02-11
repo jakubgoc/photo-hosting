f91a6649ebba8513e8ea50f82511c81b4df225c7
jakubgoc <jakubgoc98@gmail.com>
2021-02-11 20:36:11 +0100

    first springboot init

---
+++ src/main/java/com/goc/photohosting/PhotoHostingApplication.java
@@ -0,0 +1,13 @@
+package com.goc.photohosting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoHostingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoHostingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goc/photohosting/PhotoHostingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goc.photohosting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoHostingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
